d9bb84a870d2ceca4377bbc0cb1cc938f4cf26c8
Parminder Klair <perminder.klair@gmail.com>
2017-08-06 19:17:14 +0530

    added prettier

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import Link from 'next/link';
 
-export default () => (
-    <div className="ui container">
-        <div className="ui borderless stackable menu">
-            <div className="item">
-                <Link href="/">
-                    <a><img className="ui fluid image logo" src="static/images/logo.png" alt="logo" style={{ width: '66px' }} /></a>
-                </Link>
-            </div>
-            <Link href="/"><a className="item">Home</a></Link>
-            <Link href="/about"><a className="item">About</a></Link>
-        </div>
-    </div>
-);
+export default () =>
+	<div className="ui container">
+		<div className="ui borderless stackable menu">
+			<div className="item">
+				<Link href="/">
+					<a>
+						<img
+							className="ui fluid image logo"
+							src="static/images/logo.png"
+							alt="logo"
+							style={{ width: '66px' }}
+						/>
+					</a>
+				</Link>
+			</div>
+			<Link href="/">
+				<a className="item">Home</a>
+			</Link>
+			<Link href="/about">
+				<a className="item">About</a>
+			</Link>
+		</div>
+	</div>;
--- pages/about.js
+++ pages/about.js
@@ -4,15 +4,12 @@ import Layout from '../components/Layout';
 
 const TITLE = 'About me';
 
-export default () => (
-    <Layout title={TITLE}>
-        <div className="ui container">
-            <h3 className="ui top attached header">
-                About us
-            </h3>
-            <div className="ui attached segment">
-                <p>This is about page, coming soon!</p>
-            </div>
-        </div>
-    </Layout>
-);
+export default () =>
+	<Layout title={TITLE}>
+		<div className="ui container">
+			<h3 className="ui top attached header">About us</h3>
+			<div className="ui attached segment">
+				<p>This is about page, coming soon!</p>
+			</div>
+		</div>
+	</Layout>;
